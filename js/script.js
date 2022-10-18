cdfd739ad68c95e17489e27e1cf4e0c7e0eaffec
ezitodesign <99930503+ezitodesign@users.noreply.github.com>
2022-10-18 16:49:23 -0700

    Updates 4:49PM 10/18/22
    *Added Fonts
    *Added Picture
    *Added placeholder paragraph

--- js/script.js
+++ js/script.js
@@ -6,4 +6,4 @@ var demo4 = new CircleType(document.getElementById("circletext"));
 window.addEventListener("resize", function updateRadius() {
   demo4.radius(demo4.element.offsetWidth / 2);
 });
-updateRadius(180);
+updateRadius(90);
