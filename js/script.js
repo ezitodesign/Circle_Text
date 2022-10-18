ca86c2d01f1d4c0bc1697fe1513ba62e0b827e47
ezitodesign <99930503+ezitodesign@users.noreply.github.com>
2022-10-18 15:21:26 -0700

    Additions
    *loaded font
    *loaded circle text

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 // My Scripts
 
 console.log('Page Loaded');
+
+var demo4 = new CircleType(document.getElementById("circletext"));
+window.addEventListener("resize", function updateRadius() {
+  demo4.radius(demo4.element.offsetWidth / 2);
+});
+updateRadius(180);
